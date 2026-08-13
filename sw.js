cf68365386311d0f21a3bf2036ae5f739e23f59b
Weijie39Lu <weijielu39@gmail.com>
2026-08-13 11:38:26 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "lumi-u1-v6";
+const CACHE = "lumi-u1-v7";
 const FILES = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./assets/lumi-mascot.png", "./assets/icon-192.png", "./assets/icon-512.png"];
 self.addEventListener("install", (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(FILES)).then(() => self.skipWaiting())));
 self.addEventListener("activate", (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))).then(() => self.clients.claim())));
